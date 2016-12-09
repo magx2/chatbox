3a73372ae82698b9e6115bb15b12b6949be4e8d3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-09 10:31:05 +0100

    rename

--- src/main/java/pl/grzeslowski/chatbox/ChatBotApplication.java
+++ src/main/java/pl/grzeslowski/chatbox/ChatBotApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatbotApplication {
+public class ChatBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatbotApplication.class, args);
+		SpringApplication.run(ChatBotApplication.class, args);
 	}
 }
--- src/test/java/pl/grzeslowski/chatbox/ChatBotApplicationTests.java
+++ src/test/java/pl/grzeslowski/chatbox/ChatBotApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ChatbotApplicationTests {
+public class ChatBotApplicationTests {
 
 	@Test
 	public void contextLoads() {
