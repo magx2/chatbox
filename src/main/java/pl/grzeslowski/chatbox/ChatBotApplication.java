1e1f0d4b3364c67df0266775f8bf53abad75c7a6
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-09 12:46:53 +0100

    component scan

--- src/main/java/pl/grzeslowski/chatbox/ChatBotApplication.java
+++ src/main/java/pl/grzeslowski/chatbox/ChatBotApplication.java
@@ -2,8 +2,10 @@ package pl.grzeslowski.chatbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = ChatBotApplication.class)
 public class ChatBotApplication {
 
 	public static void main(String[] args) {
