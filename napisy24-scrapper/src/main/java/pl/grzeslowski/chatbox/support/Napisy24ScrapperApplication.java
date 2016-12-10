591f3c97073f1139fa79bce37bd3b0fff420a404
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-10 10:35:11 +0100

    Naspiy24 scaprapper

---
+++ napisy24-scrapper/src/main/java/pl/grzeslowski/chatbox/support/Napisy24ScrapperApplication.java
@@ -0,0 +1,12 @@
+package pl.grzeslowski.chatbox.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Napisy24ScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Napisy24ScrapperApplication.class, args);
+	}
+}
