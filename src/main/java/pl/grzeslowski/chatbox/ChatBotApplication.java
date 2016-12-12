d01a7f165812f27b9763c66b8a789a13c6102bf2
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-12 23:22:14 +0100

    celaning

--- src/main/java/pl/grzeslowski/chatbox/ChatBotApplication.java
+++ src/main/java/pl/grzeslowski/chatbox/ChatBotApplication.java
@@ -1,6 +1,5 @@
 package pl.grzeslowski.chatbox;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,15 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import pl.grzeslowski.chatbox.rnn.trainer.Trainer;
 
-@SuppressWarnings("ALL")
 @SpringBootApplication
 @ComponentScan(basePackageClasses = ChatBotApplication.class)
 public class ChatBotApplication implements CommandLineRunner{
-	private static final Logger log = org.slf4j.LoggerFactory.getLogger(ChatBotApplication.class);
+    private final Trainer trainer;
 
 	@Autowired
-    private Trainer trainer;
-
+    public ChatBotApplication(Trainer trainer) {
+        this.trainer = trainer;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatBotApplication.class, args);
