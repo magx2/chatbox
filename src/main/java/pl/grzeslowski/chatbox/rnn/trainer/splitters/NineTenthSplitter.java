6bf54f352726ff94b6d5c76334ef30e47cd16bd7
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-13 00:52:47 +0100

    this random should be shared across all NineTenthSplitter.java obj

--- src/main/java/pl/grzeslowski/chatbox/rnn/trainer/splitters/NineTenthSplitter.java
+++ src/main/java/pl/grzeslowski/chatbox/rnn/trainer/splitters/NineTenthSplitter.java
@@ -8,28 +8,27 @@ import pl.grzeslowski.chatbox.misc.RandomFactory;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 
 @Service
 class NineTenthSplitter implements TestSetSplitter {
     private static final Logger log = LoggerFactory.getLogger(NineTenthSplitter.class);
-    private final RandomFactory randomFactory;
+    private Random random;
 
     @Autowired
     public NineTenthSplitter(RandomFactory randomFactory) {
-        this.randomFactory = checkNotNull(randomFactory);
+        this.random = randomFactory.getObject();
     }
 
     @Override
     public <T> LearningSets<T> splitIntoSets(List<T> all) {
         log.info("Splitting 9/10");
-        Collections.shuffle(all, randomFactory.getObject());
+        Collections.shuffle(all, random);
         int splitPoint = all.size() * 9 / 10;
         return new LearningSets<>(
                 all.subList(0, splitPoint).stream(),
                 all.subList(splitPoint, all.size()).stream()
         );
     }
-
 }
