e128758882a6581d319834676ae2dfb438b5ca03
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-15 22:43:03 +0100

    multiple changes

--- src/main/java/pl/grzeslowski/chatbox/rnn/trainer/TrainerImpl.java
+++ src/main/java/pl/grzeslowski/chatbox/rnn/trainer/TrainerImpl.java
@@ -1,11 +1,9 @@
 package pl.grzeslowski.chatbox.rnn.trainer;
 
-import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.ui.stats.StatsListener;
-import org.deeplearning4j.ui.storage.FileStatsStorage;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -17,10 +15,12 @@ import pl.grzeslowski.chatbox.dialogs.Dialog;
 import pl.grzeslowski.chatbox.dialogs.DialogLoader;
 import pl.grzeslowski.chatbox.dialogs.VecDialog;
 import pl.grzeslowski.chatbox.dialogs.VecDialogFunction;
+import pl.grzeslowski.chatbox.rnn.NeuralNetworkLoader;
+import pl.grzeslowski.chatbox.rnn.NeuralNetworkSaver;
 import pl.grzeslowski.chatbox.rnn.RnnEngine;
 import pl.grzeslowski.chatbox.rnn.trainer.splitters.TestSetSplitter;
 
-import java.io.File;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -32,6 +32,10 @@ class TrainerImpl implements Trainer {
     private final DialogLoader dialogLoader;
     private final RnnEngine rnnEngine;
     private final VecDialogFunction vecDialogFunction;
+    private final NeuralNetworkSaver neuralNetworkSaver;
+    private final NeuralNetworkLoader neuralNetworkLoader;
+    private final StatsListener statsListener;
+    private final ScoreIterationListener scoreIterationListener;
 
     @Value("${rnn.maxWordsInDialog}")
     private int maxWordsInDialog;
@@ -43,10 +47,16 @@ class TrainerImpl implements Trainer {
     private int layerSize;
 
     @Autowired
-    public TrainerImpl(DialogLoader dialogLoader, RnnEngine rnnEngine, VecDialogFunction vecDialogFunction) {
+    public TrainerImpl(DialogLoader dialogLoader, RnnEngine rnnEngine, VecDialogFunction vecDialogFunction,
+                       NeuralNetworkSaver neuralNetworkSaver, NeuralNetworkLoader neuralNetworkLoader,
+                       StatsListener statsListener, ScoreIterationListener scoreIterationListener) {
         this.dialogLoader = checkNotNull(dialogLoader);
         this.rnnEngine = checkNotNull(rnnEngine);
         this.vecDialogFunction = checkNotNull(vecDialogFunction);
+        this.neuralNetworkSaver = checkNotNull(neuralNetworkSaver);
+        this.neuralNetworkLoader = checkNotNull(neuralNetworkLoader);
+        this.statsListener = checkNotNull(statsListener);
+        this.scoreIterationListener = checkNotNull(scoreIterationListener);
     }
 
     private DataSetIterator createDateSetIterator(Stream<Stream<Dialog>> stream) {
@@ -60,52 +70,48 @@ class TrainerImpl implements Trainer {
 
     @Override
     public MultiLayerNetwork trainAndTest() {
-        final TestSetSplitter.LearningSets<Stream<Dialog>> learningSets = dialogLoader.loadTrainData();
 
-        final MultiLayerNetwork net = rnnEngine.buildEngine();
+        final MultiLayerNetwork net = loadModel();
         log.info("Initializing model");
         net.init();
+        net.setListeners(statsListener, scoreIterationListener);
 
-        // todo
-        //Initialize the user interface backend
-        UIServer uiServer = UIServer.getInstance();
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            final TestSetSplitter.LearningSets<Stream<Dialog>> learningSets = dialogLoader.loadTrainData();
+            final DataSetIterator train = createDateSetIterator(learningSets.getTrainingSet());
+            final DataSetIterator test = createDateSetIterator(learningSets.getTestingSet());
 
-        //Configure where the network information (gradients, score vs. time etc) is to be stored. Here: store in memory.
-        final File file = new File("D:\\Programowanie\\deep_learning\\chatbox\\data\\ui_service.bin");
-        //noinspection ResultOfMethodCallIgnored
-        file.delete();
-        StatsStorage statsStorage = new FileStatsStorage(file);         //Alternative: new FileStatsStorage(File), for saving and loading later
-//        StatsStorage statsStorage = new InMemoryStatsStorage();          //Alternative: new FileStatsStorage(File), for saving and loading later
+            log.info("Starting learning, epoch {}", epoch);
+            net.fit(train);
 
-        //Attach the StatsStorage instance to the UI: this allows the contents of the StatsStorage to be visualized
-        uiServer.attach(statsStorage);
+            log.info("Saving model");
+            neuralNetworkSaver.save(net);
 
-        //Then add the StatsListener to collect this information from the network, as it trains
-        net.setListeners(new StatsListener(statsStorage));
-        // todo
+            log.info("Starting evaluation:");
 
-//        net.setListeners(new ScoreIterationListener(200));
+            Evaluation evaluation = new Evaluation();
+            while (test.hasNext()) {
+                DataSet t = test.next();
+                INDArray features = t.getFeatures();
+                INDArray labels = t.getLabels();
+                INDArray inMask = t.getFeaturesMaskArray();
+                INDArray outMask = t.getLabelsMaskArray();
+                INDArray predicted = net.output(features, false, inMask, outMask);
 
-        final DataSetIterator train = createDateSetIterator(learningSets.getTrainingSet());
-        final DataSetIterator test = createDateSetIterator(learningSets.getTestingSet());
-
-        log.info("Starting learning");
-        net.fit(train);
-        log.info("Training complete. Starting evaluation:");
-
-        Evaluation evaluation = new Evaluation();
-        while (test.hasNext()) {
-            DataSet t = test.next();
-            INDArray features = t.getFeatures();
-            INDArray labels = t.getLabels();
-            INDArray inMask = t.getFeaturesMaskArray();
-            INDArray outMask = t.getLabelsMaskArray();
-            INDArray predicted = net.output(features, false, inMask, outMask);
-
-            evaluation.evalTimeSeries(labels, predicted, outMask);
+                evaluation.evalTimeSeries(labels, predicted, outMask);
+            }
+            log.info("Evaluation output:\n{}", evaluation.stats(true));
         }
-        log.info("Evaluation output:\n{}", evaluation.stats());
 
         return net;
+    }
+
+    private MultiLayerNetwork loadModel() {
+        final Optional<MultiLayerNetwork> model = neuralNetworkLoader.load();
+        if (model.isPresent()) {
+            return model.get();
+        } else {
+            return rnnEngine.buildEngine();
+        }
     }
 }
